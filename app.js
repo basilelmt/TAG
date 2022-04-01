aa1267db650fda32b3e5a3b4df3666fbdebbbdf3
Basile LAMOTTE <Basile-LAMOTTE>
2022-04-01 15:40:42 +0200

    the colisions works but are cursed

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
 
 const app = new PIXI.Application();
 const graphics = new PIXI.Graphics();
+const OFFSETX = -322
+const OFFSETY = -54
 
 document.body.appendChild(app.view);
 
@@ -27,22 +29,22 @@ var map = [
     "111111111111100011111111111111111100111111111111111111110001111111111111",
     "111111111111011111111111111111111100111111111111111111111110111111111111",
     "111111111111011111111111111111111111111111111111111111111110111111111111",
-    "111111111111011111111111111111111111111111111110000011111110111111111111",
+    "111111111111011111111111111111111111111111111110000011111110111111111111", //15
     "111111111111011111111111111111111111111111111111111111111110111111111111",
     "111111111111011111111111111111111111111111111111111111111110111111111111",
     "111111111111011111111111111111111111111111111111111111111110111111111111",
     "111111111111000111111111111100000000111111111111111111111110111111111111",
-    "111111111111011111111111111111111111111111111111111111111110111111111111",
+    "111111111111011111111111111111111111111111111111111111111110111111111111", //20
     "111111111111011111111111111111111111111111111111111111111110111111111111",
     "111111111111011111111111111111111111111111111111100011111110111111111111",
-    "111111111111011111110111111111111111111111111111111111111110111111111111",
+    "111111111111011111110111111111111111111111111111111111111110111111111111", // 23
     "111111111111011111111000111111111111111111111111111111111110111111111111",
     "111111111111011111111111111111110011111111111111111111111110111111111111",
     "111111111111100011111111111111110011111111111111111111110001111111111111",
     "111111111111111011111111111111111111111111100011111111110111111111111111",
     "111111111111111101111111111111111111111111111111111111101111111111111111",
     "111111111111111110111111111111111111111111111111111111011111111111111111",
-    "111111111111111111001111111111111111111111111111111100111111111111111111",
+    "111111111111111111001111111111111111111111111111111100111111111111111111", //30
     "111111111111111111110001111111111111111111111111100011111111111111111111",
     "111111111111111111111110000000000000000000000000011111111111111111111111",
     "111111111111111111111111111111111111111111111111111111111111111111111111",
@@ -93,53 +95,92 @@ running.animationSpeed = 0.2;
 running.play();
 
 let character = {
-    x: app.screen.width/2.5, y: app.screen.height/1.18,
+    x: app.screen.width/2.5, y: app.screen.height/1.178,
     vx: 5, vy: 0,
     direction: 0,
     activeAnim: standing,
+    touchingGround: true,
     jumped: false
 };
 
-app.ticker.add((time) => {
-    document.addEventListener('keydown', function(e) {
-        if (e.key === "ArrowRight") {
-            character.direction = 1;
-            character.activeAnim = running
-            running.scale.x = 4;
-            standing.scale.x = 4;
-        }
-        if (e.key === "ArrowLeft") {
-            character.direction = -1;
-            character.activeAnim = running
-            running.scale.x = -4;
-            standing.scale.x = -4;
-        }
-        if (e.key === " " && !character.jumped) {
-            character.jumped = true;
-            character.vy = 20;
-        }
-    })
+const coliding = coord => map[coord[1]][coord[0]] == 0
 
-    document.addEventListener('keyup', function(e) {
-        if (e.key === "ArrowRight" && character.direction === 1) {
-            character.direction = 0;
-            character.activeAnim = standing
-        }
-        if (e.key === "ArrowLeft" && character.direction === -1) {
-            character.direction = 0;
-            character.activeAnim = standing
-        }
-    })
-    if (character.direction === 1) {
+function testColision(hitbox, index, movement) {
+
+    for (let i = 0; i < 4; i++)
+        hitbox[i][index] = hitbox[i][index] + movement;
+    if (hitbox.some(coliding))
+        return false
+    return true
+}
+
+document.addEventListener('keydown', function(e) {
+    if (e.key === "ArrowRight") {
+        character.direction = 1;
+        character.activeAnim = running
+        running.scale.x = 4;
+        standing.scale.x = 4;
+    }
+    if (e.key === "ArrowLeft") {
+        character.direction = -1;
+        character.activeAnim = running
+        running.scale.x = -4;
+        standing.scale.x = -4;
+    }
+    if (e.key === " " && !character.jumped) {
+        character.touchingGround = false;
+        character.jumped = true;
+        character.vy = 25;
+    }
+})
+
+document.addEventListener('keyup', function(e) {
+    if (e.key === "ArrowRight" && character.direction === 1) {
+        character.direction = 0;
+        character.activeAnim = standing
+    }
+    if (e.key === "ArrowLeft" && character.direction === -1) {
+        character.direction = 0;
+        character.activeAnim = standing
+    }
+})
+
+app.ticker.add((time) => {
+    let hitbox = [
+        [Math.round((character.x+OFFSETX)/32)+21, Math.round((character.y+OFFSETY)/32)+5],
+        [Math.round((character.x+OFFSETX)/32)+21, Math.round((character.y+OFFSETY)/32)+6],
+        [Math.round((character.x+OFFSETX)/32)+21, Math.round((character.y+OFFSETY)/32)+7],
+        [Math.round((character.x+OFFSETX)/32)+21, Math.round((character.y+OFFSETY)/32)+8]
+    ]
+    // console.log(
+    //     map[hitbox[0][1]][hitbox[0][0]],
+    //     map[hitbox[1][1]][hitbox[1][0]],
+    //     map[hitbox[2][1]][hitbox[2][0]],
+    //     map[hitbox[3][1]][hitbox[3][0]],
+    // )
+    if (character.direction === 1 && testColision(hitbox, 0, 1)) {
         character.x += character.vx;
     }
-    if (character.direction === -1) {
+    if (character.direction === -1 && testColision(hitbox, 0, -1)) {
         character.x -= character.vx;
     }
-    if (character.vy > 0) {
+    if (character.vy > 0 && testColision(hitbox, 1, -1)) {
         character.y -= character.vy;
         character.vy -= 1;
     }
+    if (!character.touchingGround) {
+        character.vy -= 1;
+        if (character.vy < 0)
+            character.y -= character.vy;
+    }
+    if (character.touchingGround) {
+        character.vy = 0;
+    }
+    if (character.touchingGround)
+        jumped = false;
+    console.log(character.vy);
+    character.touchingGround = testColision(hitbox, 1, 1) ? false : true;
+    console.log(character.touchingGround);
     if (character.vy === 0)
         character.jumped = false;
     blur.velocity = [character.vx, character.vy]
@@ -149,12 +190,6 @@ app.ticker.add((time) => {
     standing.visible = false
     character.activeAnim.visible = true
     app.stage.addChild(character.activeAnim);
-    graphics.beginFill(0xDE3249);
-    graphics.drawRect(0, 0, 32, 32);
-    graphics.drawRect(0, 0, 32, 32);
-    graphics.drawRect(0, 0, 32, 32);
-    graphics.endFill();
-    // app.stage.addChild(graphics);
 });
 
 // ------ Title ------
